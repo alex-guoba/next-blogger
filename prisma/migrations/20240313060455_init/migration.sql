d82ca7e4f45abd7af0ba88de44cf5b0963e3dc54
alex.guoba <alex.guoba@gmail.com>
2024-03-13 14:53:42 +0800

    feat: change default db to prisma

---
+++ prisma/migrations/20240313060455_init/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "dBProperties" (
+    "database_id" TEXT NOT NULL,
+    "result" BYTEA,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "dBProperties_pkey" PRIMARY KEY ("database_id")
+);
+
+-- CreateTable
+CREATE TABLE "dBRows" (
+    "database_id" TEXT NOT NULL,
+    "query" TEXT,
+    "result" BYTEA,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "dBRows_pkey" PRIMARY KEY ("database_id")
+);
+
+-- CreateTable
+CREATE TABLE "pageProperties" (
+    "page_id" TEXT NOT NULL,
+    "result" BYTEA,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "pageProperties_pkey" PRIMARY KEY ("page_id")
+);
+
+-- CreateTable
+CREATE TABLE "blockChildren" (
+    "block_id" TEXT NOT NULL,
+    "result" BYTEA,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "blockChildren_pkey" PRIMARY KEY ("block_id")
+);
