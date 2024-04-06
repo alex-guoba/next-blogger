b588fb3ad2df0e012884d839fb3b370c15f63929
alex.guoba <alex.guoba@gmail.com>
2024-04-06 17:12:28 +0800

    feat: add copyright

--- config/meta.ts
+++ config/meta.ts
@@ -9,7 +9,6 @@ export const siteMeta = {
   siteLogo: "/static/images/logo.png",
   socialBanner: "/static/images/twitter-card.png",
 
-  // mastodon: 'https://mastodon.social/@mastodonuser',
   email: "address@yoursite.com",
   github: "https://github.com/alex-guoba/next-blogger",
   twitter: "https://twitter.com/gelco",
@@ -19,28 +18,8 @@ export const siteMeta = {
   // threads: 'https://www.threads.net',
   // instagram: 'https://www.instagram.com',
   locale: "en-US",
-  // analytics: {
-  //   // If you want to use an analytics provider you have to add it to the
-  //   // content security policy in the `next.config.js` file.
-  //   // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-  //   umamiAnalytics: {
-  //     // We use an env variable for this site to avoid other users cloning our analytics ID
-  //     umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //     // You may also need to overwrite the script if you're storing data in the US - ex:
-  //     // src: 'https://us.umami.is/script.js'
-  //     // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
-  //   },
-  //   // plausibleAnalytics: {
-  //   //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-  //   // },
-  //   // simpleAnalytics: {},
-  //   // posthogAnalytics: {
-  //   //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   // },
-  //   // googleAnalytics: {
-  //   //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-  //   // },
-  // },
+  copyright: "",
+  
   // newsletter: {
   //   // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
   //   // Please add your .env file and modify it according to your selection
