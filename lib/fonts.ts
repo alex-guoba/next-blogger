975628d740f4b66df691db97411b6def85f52aec
alex.guoba <alex.guoba@gmail.com>
2026-05-12 17:17:12 +0800

    feat: optimize font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,25 +1,23 @@
 import { Fira_Mono as FontMono, Noto_Sans_SC as FontSans, Noto_Serif_SC as FontSerif } from "next/font/google";
-// import localFont from "next/font/local"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
   variable: "--font-sans",
+  display: "swap",
 });
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
   weight: ["400", "700"],
+  display: "swap",
 });
 
 export const fontSerif = FontSerif({
   subsets: ["latin"],
   variable: "--font-serif",
   weight: ["400", "700"],
+  display: "swap",
 });
 
-// // Font files can be colocated inside of `pages`
-// export const fontHeading = localFont({
-//   src: "../assets/fonts/CalSans-SemiBold.woff2",
-//   variable: "--font-heading",
-// })
